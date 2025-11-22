6785fcf94fd5da709c8f482352b52d4dd4b16945
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-11-22 12:55:39 +0300

    initial project

---
+++ src/main/java/ru/utmn/currency_rate_parser/CurrencyRateParserApplication.java
@@ -0,0 +1,13 @@
+package ru.utmn.currency_rate_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateParserApplication.class, args);
+        System.out.println("Ok");
+	}
+}
