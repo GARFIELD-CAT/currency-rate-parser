5d7a7af1a43ec7b78fc8afe5a3e939cf87b25e3b
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-11-23 21:23:16 +0300

    remove extra imports

--- src/main/java/ru/utmn/currency_rate_parser/CurrencyRateParserApplication.java
+++ src/main/java/ru/utmn/currency_rate_parser/CurrencyRateParserApplication.java
@@ -2,7 +2,6 @@ package ru.utmn.currency_rate_parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.utmn.currency_rate_parser.service.aggregator.CurrencyRateTaskProducer;
 
 @SpringBootApplication
 public class CurrencyRateParserApplication {
--- src/main/java/ru/utmn/currency_rate_parser/repository/CurrencyRateRepository.java
+++ src/main/java/ru/utmn/currency_rate_parser/repository/CurrencyRateRepository.java
@@ -1,7 +1,5 @@
 package ru.utmn.currency_rate_parser.repository;
 
-import org.hibernate.query.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
