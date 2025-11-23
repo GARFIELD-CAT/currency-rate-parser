5ed89134f23d16af8a47aaa55614a5d2b61e63d3
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-11-24 01:43:03 +0300

    fix controller

--- src/main/java/ru/utmn/currency_rate_parser/model/CurrencyHistoryRatesRequestBody.java
+++ src/main/java/ru/utmn/currency_rate_parser/model/CurrencyHistoryRatesRequestBody.java
@@ -6,13 +6,15 @@ import lombok.Setter;
 
 import java.util.List;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 @Getter
 @Setter
 public class CurrencyHistoryRatesRequestBody {
-    @Schema(description = "Запустить ли обнровление данных вручную за выбранны день", example = "False")
+    @Schema(requiredMode = REQUIRED, description = "Запустить ли обновление данных вручную за выбранный день", example = "False")
     private Boolean manualParse;
-    @Schema(description = "Запрашиваемая дата в формате YYYY-MM-DD", example = "2025-11-22")
+    @Schema(requiredMode = REQUIRED, description = "Запрашиваемая дата в формате YYYY-MM-DD", example = "2025-11-22")
     private String parseDate;
-    @Schema(description = "Список названий криптовалют, например: [\"BTC\", \"ETH\", \"USDD\"]", example = "[\"BTC\", \"ETH\", \"USDD\"]")
+    @Schema(requiredMode = REQUIRED, maxLength = 100, description = "Список названий криптовалют, например: [\"BTC\", \"ETH\", \"USDD\"]", example = "[\"BTC\", \"ETH\", \"USDD\"]")
     private List<String> currencySymbols;
 }
